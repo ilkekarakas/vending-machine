4816d716f85d89125f3e059948aba9113c280f1c
İlke Bolat <h.ilke500@hotmail.com>
2025-01-12 12:49:07 +0300

    Update vite config and build

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/vending-machine/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
 })
