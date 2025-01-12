8f41f1e92519125be6c5ef84a4405979e88e93e8
İlke Bolat <h.ilke500@hotmail.com>
2025-01-12 16:36:15 +0300

    fix issues and deployment build

--- src/redux/slices/product-slice.ts
+++ src/redux/slices/product-slice.ts
@@ -1,7 +1,5 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { Product, ProductState } from '../../types/general-types';
-import { saveState } from '../../utils/localStorage';
-import { store } from '../store';
 
 const initialState: ProductState = {
   products: [
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,4 +1,4 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import machineReducer from './slices/machine-slice';
 import productReducer from './slices/product-slice';
 import paymentReducer from './slices/payment-slice';
@@ -6,19 +6,22 @@ import { loadState, saveState } from '../utils/localStorage';
 
 const persistedState = loadState();
 
+const rootReducer = combineReducers({
+  machine: machineReducer,
+  product: productReducer,
+  payment: paymentReducer,
+});
+
 export const store = configureStore({
-  reducer: {
-    machine: machineReducer,
-    product: productReducer,
-    payment: paymentReducer,
-  },
+  reducer: rootReducer,
   preloadedState: persistedState,
 });
+
+export type RootState = ReturnType<typeof store.getState>;
 
 // Her state değişikliğinde localStorage'a kaydet
 store.subscribe(() => {
   saveState(store.getState());
 });
 
-export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
